770f14fb90e3c6dbd9abeac9ead56da643f24815
Christopher Sparrowgrove <christopher@sparrowgrove.com>
2012-10-06 09:25:12 -0600

    Inital Upload

---
+++ apps/login/login.php
@@ -0,0 +1,32 @@
+<?php
+/*
+ * -----------------INFORMATION & LICENSING-----------------
+ * 
+ *      AUTHOR: Christopher Sparrowgrove
+ *     WEBSITE: http://christopher.sparrowgrove.com
+ *        NAME: Index File
+ *        FILE: index.php
+ *    LANGUAGE: PHP Hypertext Processor (PHP)
+ * DESCRIPTION: This is a Test
+ *     LICENSE: Please Read Included License File. If One Was Not Provided E-Mail Author For A Copy.
+ *   COPYRIGHT: ©Copyright 2012 - All Rights Reserved   
+ *      
+ */
+
+/********CONFIGURATION FILE**********/
+//require_once('config/config.php');
+/********CONFIGURATION FILE**********/
+
+?>
+
+<html>
+ <head><title>User Login</title></head>
+ 
+ <body>
+  <div id="loginform">
+   <input type="text" name="username" id="username" />
+   <BR />
+   <input type="password" name="password" id="password" />
+  </div>
+ </body>
+</html>
