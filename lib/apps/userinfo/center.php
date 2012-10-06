be06d5aa63b182924047170b470d10f3a3895be7
Christopher Sparrowgrove <christopher@sparrowgrove.com>
2012-10-06 09:21:47 -0600

    Setting Default Selection
    This update selects writeups as the default page to get.

--- lib/apps/userinfo/center.php
+++ lib/apps/userinfo/center.php
@@ -3,7 +3,9 @@ switch($_GET['center'])
 {
  case "weberbasin":
   if(isset($_GET['record'])) {include('lib/apps/userinfo/record.php');}
-  else {include($apps_directory."/Job Corps/Weber Basin/index.php");}
+  else {//include($apps_directory."/Job Corps/Weber Basin/index.php");
+       HEADER('LOCATION: http://localhost/?app=userinfo&type=jobcorps&center=weberbasin&record=writeups');
+  }
   break;
  
  case "collbran":
